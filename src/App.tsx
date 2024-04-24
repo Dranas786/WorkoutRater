4bde825452c039302d29de3fad5b169e67c01fc4
Dranas786 <dranas2002@gmail.com>
2024-04-24 11:49:53 -0600

    more stuff

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,22 @@ import { Container } from "react-bootstrap"
 import { Home } from "./pages/Home"
 import { Store } from "./pages/Store"
 import { About } from "./pages/About"
+import { Navbar } from "./components/Navbar.tsx"
 
 function App() {
-  // Need to put container on the same line as return as it returns null
-  return <Container>  
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/store" element={<Store />}/>
-      <Route path="/about" element={<About />}/>
-    </Routes>
-  </Container>
+  // Need to put container on the same line as return as it returns null or can put all in brackets but bracket 1 should be on same line
+  return (
+  <>
+    <Navbar/>
+    <Container className="mb-4">  
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/store" element={<Store />}/>
+        <Route path="/about" element={<About />}/>
+      </Routes>
+    </Container>
+  </>
+  )
 }
 
 export default App
